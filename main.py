537c8c63d26ae068775c9861a6b4f3907aa084df
Jacob Schmidt <schhmidt.jacob3@gmail.com>
2019-05-28 14:19:36 -0400

    updated notes

--- main.py
+++ main.py
@@ -9,18 +9,15 @@ ID
 Content
 Date/Time
 '''
+
 notebook = []
 counter = 1
-note_id = counter 
-counter += 1
-
-
 now = date.today()
-print(now)
-content = 'This is content'
 
-note = (note_id, str(now), content)
-print(note)
-
-notebook.append(note)
-print(notebook)
+while counter <= 5: 
+    note_id = counter 
+    counter += 1
+    content = input('What is the note? \n >')
+    note = (note_id, str(now), content)
+    notebook.append(note)
+    print(notebook)
